0b7687823391bc86dc65d8fcdbada592e0affb4c
koshakji <majd@koshakji.net>
2024-04-20 12:18:49 +0200

    Initial Commit

---
+++ BetterFlashcards/BetterFlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterFlashcardsApp.swift
+//  BetterFlashcards
+//
+//  Created by Majd Koshakji on 20/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterFlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
