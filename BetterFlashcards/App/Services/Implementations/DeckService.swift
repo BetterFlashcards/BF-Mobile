14f9cf105c5cb9661faa416345c67f1906102eb4
Majd Koshakji <majd@koshakji.net>
2024-04-27 19:09:13 +0200

    Added missing events

--- BetterFlashcards/App/Services/Implementations/DeckService.swift
+++ BetterFlashcards/App/Services/Implementations/DeckService.swift
@@ -25,14 +25,20 @@ class DeckService: DeckServiceProtocol {
     }
     
     func create(_ deck: Deck) async throws -> Deck {
-        try await deckRepo.create(deck: deck)
+        let deck = try await deckRepo.create(deck: deck)
+        eventSubject.send(.added(deck))
+        return deck
     }
     
     func update(_ deck: Deck) async throws -> Deck {
-        try await deckRepo.update(deck: deck)
+        let deck = try await deckRepo.update(deck: deck)
+        eventSubject.send(.updated(deck))
+        return deck
     }
     
     func delete(_ deck: Deck) async throws -> Deck {
         try await deckRepo.delete(deck: deck)
+        eventSubject.send(.deleted(deck))
+        return deck
     }
 }
