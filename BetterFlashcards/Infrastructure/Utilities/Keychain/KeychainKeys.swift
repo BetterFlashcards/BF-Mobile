5999f62e7b076511703bbd5ec28a313341cb7b42
koshakji <majd@koshakji.net>
2024-04-27 12:24:23 +0200

    Added Keychain utilities

---
+++ BetterFlashcards/Infrastructure/Utilities/Keychain/KeychainKeys.swift
@@ -0,0 +1,14 @@
+//
+//  KeychainKeys.swift
+//  BetterFlashcards
+//
+//  Created by Majd Koshakji on 20/04/2024.
+//
+
+import Foundation
+
+enum KeychainKeys: String {
+    case token
+    case username
+    case userID
+}
