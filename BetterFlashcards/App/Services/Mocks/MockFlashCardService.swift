1263ee80c9350f100534ee793914e074193fbb19
Majd Koshakji <majd@koshakji.net>
2024-04-27 19:44:34 +0200

    Implemented real and mock flashcard services

--- BetterFlashcards/App/Services/Mocks/MockFlashCardService.swift
+++ BetterFlashcards/App/Services/Mocks/MockFlashCardService.swift
@@ -6,7 +6,69 @@
 //
 
 import Foundation
+import Combine
 
-class MockFlashCardService: FlashCardServiceProtocol {
+actor MockFlashCardService: FlashCardServiceProtocol {
+    private var deckCards: [Deck.ID: [FlashCard]] = Dictionary(
+        grouping: (0...100)
+            .flatMap { deckID in
+                (deckID...100).map { cardID in
+                        FlashCard(
+                            id: cardID,
+                            frontWord: "Front-\(cardID)",
+                            backWord: "Back-\(cardID)",
+                            deckID: deckID,
+                            isDraft: false
+                        )
+                    }
+                
+            },
+        by: \.deckID
+        )
+    private let eventSubject = PassthroughSubject<FlashCardServiceEvent, Never>()
+    private let pageSize = 20
     
+    nonisolated var eventPublisher: AnyPublisher<FlashCardServiceEvent, Never> {
+        self.eventSubject.eraseToAnyPublisher()
+    }
+    
+    func getList(for deckID: Deck.ID) async throws -> [FlashCard] {
+        return deckCards[deckID] ?? []
+    }
+    
+    func create(_ card: FlashCard) async throws -> FlashCard {
+        guard var deck = deckCards[card.deckID] else { throw ServiceError.notFound }
+        deck.append(card)
+        deckCards[card.deckID] = deck
+        eventSubject.send(.added(card))
+        return card
+    }
+    
+    func update(_ card: FlashCard) async throws -> FlashCard {
+        guard
+            let idx = deckCards[card.deckID]?.firstIndex(of: card)
+        else { throw ServiceError.notFound }
+
+        deckCards[card.deckID]?[idx] = card
+        eventSubject.send(.updated(card))
+        return card
+    }
+    
+    func delete(_ card: FlashCard) async throws -> FlashCard {
+        guard
+            let idx = deckCards[card.deckID]?.firstIndex(of: card)
+        else { throw ServiceError.notFound }
+        
+        deckCards[card.deckID]?.remove(at: idx)
+        eventSubject.send(.deleted(card))
+        return card
+    }
+        
+    func dueList(for deckID: Deck.ID) async throws -> [FlashCard] {
+        try await getList(for: deckID)
+    }
+    
+    func save(practice: FlashCardPractice) async throws {
+        
+    }
 }
--- BetterFlashcards/App/Services/Protocols/FlashCardServiceProtocol.swift
+++ BetterFlashcards/App/Services/Protocols/FlashCardServiceProtocol.swift
@@ -6,8 +6,18 @@
 //
 
 import Foundation
+import Combine
 
 protocol FlashCardServiceProtocol {
+    var eventPublisher: AnyPublisher<BasicServiceEvent<FlashCard>, Never> { get }
+
+    func getList(for deckID: Deck.ID) async throws -> [FlashCard]
+    func create(_: FlashCard) async throws -> FlashCard
+    func update(_: FlashCard) async throws -> FlashCard
+    func delete(_: FlashCard) async throws -> FlashCard
     
+    func dueList(for deckID: Deck.ID) async throws -> [FlashCard]
+    func save(practice: FlashCardPractice) async throws
 }
 
+typealias FlashCardServiceEvent = BasicServiceEvent<FlashCard>
