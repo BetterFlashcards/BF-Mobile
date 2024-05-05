91f00eaddc99f6cab203e1a1a73e0549e3348b58
Majd Koshakji <majd@koshakji.net>
2024-05-05 14:34:05 +0200

    Added models and query DTOs for pagination

---
+++ BetterFlashcards/Domain/PaginatedList.swift
@@ -0,0 +1,19 @@
+//
+//  PaginatedList.swift
+//  BetterFlashcards
+//
+//  Created by Majd Koshakji on 05/05/2024.
+//
+
+import Foundation
+
+struct Pagination {
+    let page: Int
+    let size: Int
+}
+
+struct PaginatedList<Item> {
+    let items: [Item]
+    let count: Int
+    let pagination: Pagination
+}
