12be04122f4aaacb188265600a9d36c23e1ba1ea
Majd Koshakji <majd@koshakji.net>
2024-05-20 14:28:19 +0200

    Fixed card event handling

--- BetterFlashcards/UI/Presenters/FlashCards/FlashCardListPresenter.swift
+++ BetterFlashcards/UI/Presenters/FlashCards/FlashCardListPresenter.swift
@@ -54,12 +54,12 @@ class FlashCardListPresenter: PaginatedListViewPresenterProtocol, ObservableObje
    
     private func handle(_ event: FlashCardServiceEvent) {
         switch event {
-        case .added(let deck):
-            viewModel.list.append(deck)
-        case .updated(let deck):
-            viewModel.list[deck.id] = deck
-        case .deleted(let deck):
-            viewModel.list.remove(id: deck.id)
+        case .added(let card):
+            viewModel.list.append(card)
+        case .updated(let card):
+            viewModel.list[id: card.id] = card
+        case .deleted(let card):
+            viewModel.list.remove(id: card.id)
         }
     }
 }
