441e1ccb195d45bc0f0b6ed2da808aaaabbadc2d
Majd Koshakji <majd@koshakji.net>
2024-05-24 17:31:51 +0200

    Added translation model

---
+++ BetterFlashcards/Domain/Translation.swift
@@ -0,0 +1,15 @@
+//
+//  Translation.swift
+//  BetterFlashcards
+//
+//  Created by Majd Koshakji on 24/05/2024.
+//
+
+import Foundation
+
+struct Translation {
+    let word: String
+    let translation: String
+}
+
+extension Translation: Codable { }
